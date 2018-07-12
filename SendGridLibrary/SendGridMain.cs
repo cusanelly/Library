d7b0a560bd225929162cb69884b4c40a1148e49a
cusanelly <cuentacus@gmail.com>
2018-07-12 18:38:53 -0400

    CognitoEmailsProjects
    - Added Cognito library for using AWS Cognito on apps.
    - Added Sendgrid and SES libraries to add email send on apps and projects.

---
+++ SendGridLibrary/SendGridMain.cs
@@ -0,0 +1,51 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SendGridLibrary
+{
+    public interface ISendGridMain {
+        Task<Response> SendEmail(string from, string subject, string body, string htmlcontent = null, string to = null, List<string> CcEmails = null);
+        Task<Response> SendEmail(SendGridMessage message);
+    }
+    public class SendGridMain : ISendGridMain
+    {
+        readonly string _ApiKey;
+        readonly SendGridClient _Client;
+        public SendGridMain(string key)
+        {
+            _ApiKey = key;
+            _Client = new SendGridClient(_ApiKey);
+        }        
+        public async Task<Response> SendEmail(string from, string subject, 
+            string body, 
+            string htmlcontent = "", 
+            string to = null, 
+            List<string> CcEmails = null)
+        {
+            //var from = new EmailAddress(from);
+            var msg = MailHelper.CreateSingleEmail(new EmailAddress(from), 
+                new EmailAddress(to), 
+                subject, 
+                body, htmlcontent);           
+            var cc = new List<EmailAddress>();
+            if (CcEmails.Count > 0)
+            {
+                CcEmails.ForEach(t => {
+                    cc.Add(new EmailAddress(t));
+                });
+                msg.AddCcs(cc);
+            }            
+            return await Send(msg);
+        }
+        public async Task<Response> SendEmail(SendGridMessage message)
+        {
+            return await Send(message);
+        }
+        private async Task<Response> Send(SendGridMessage message)
+        {
+            return await _Client.SendEmailAsync(message);
+        }
+    }
+}
